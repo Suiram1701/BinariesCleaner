ecfc6c97d6a1509e3a013c60c5ed30f7fab0c0de
Suiram1701 <110390261+Suiram1701@users.noreply.github.com>
2024-12-29 20:14:23 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace BinariesCleaner;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string directory = args.FirstOrDefault(Environment.CurrentDirectory);
+        if (!Directory.Exists(directory))
+        {
+            PrintError($"Unable to find target directory '{directory}'");
+            return;
+        }
+
+        int removedFolders = RemoveRecursively(directory);
+        PrintSuccess($"Finished. {removedFolders} folders successfully removed");
+
+        Console.ReadKey();
+    }
+
+    private static int RemoveRecursively(string directory)
+    {
+        int removedFolders = 0;
+
+        string? projectFile = Directory.GetFiles(directory).FirstOrDefault(file => file.EndsWith(".csproj"));
+        if (!string.IsNullOrEmpty(projectFile))
+        {
+            PrintSuccess($"Project file found '{projectFile}'");
+
+            string objPath = Path.Combine(directory, "obj");
+            if (Directory.Exists(objPath))
+            {
+                removedFolders++;
+                Directory.Delete(objPath, recursive: true);
+                Console.WriteLine($"Removed '{objPath}'");
+            }
+
+            string binPath = Path.Combine(directory, "bin");
+            if (Directory.Exists(binPath))
+            {
+                removedFolders++;
+                Directory.Delete(binPath, recursive: true);
+                Console.WriteLine($"Removed '{binPath}'");
+            }
+        }
+
+        foreach (string subDirectory in Directory.GetDirectories(directory))
+        {
+            removedFolders += RemoveRecursively(subDirectory);
+        }
+        return removedFolders;
+    }
+
+    private static void PrintSuccess(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+
+    private static void PrintError(string message)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
